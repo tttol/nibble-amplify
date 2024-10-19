c1bd7283dfd0ed8e60adeb2f18912e6d77dc67f3
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-10-20 07:11:44 +0900

    simplify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 "use client";
-import { Authenticator } from "@aws-amplify/ui-react";
-import Todo from "./component/Todo";
 
 export default function Home() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <>
-          <h1>Hello nibble-amplify!</h1>
-          <Todo />
-        </>
-      )}
-    </Authenticator>
+    <>
+      <p>Hello Nibble Amplify</p>
+    </>
   );
 }
